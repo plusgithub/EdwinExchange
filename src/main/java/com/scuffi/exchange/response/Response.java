1ac730f71767cf6586783cf23cde9eac7a05bb95
Scuffi <45369682+plusgithub@users.noreply.github.com>
2022-02-01 17:00:22 +0000

    Switched to Jackson again

--- src/main/java/com/scuffi/exchange/response/Response.java
+++ src/main/java/com/scuffi/exchange/response/Response.java
@@ -4,7 +4,10 @@ package com.scuffi.exchange.response;
 //import com.google.gson.GsonBuilder;
 //import com.google.gson.JsonObject;
 //import com.google.gson.JsonParser;
-import org.json.JSONObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.net.http.HttpResponse;
 
@@ -12,14 +15,25 @@ public class Response {
 
 	private final int statusCode;
 	private final String body;
-	private final JSONObject json;
+	private final JsonNode json;
 //	public final Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
 
 	public Response(HttpResponse<String> response) {
+		JsonNode json1;
 		this.statusCode = response.statusCode();
 		this.body = response.body();
 //		this.json = gson.fromJson(this.body, JsonObject.class);
-		this.json = new JSONObject(this.body);
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			json1 = mapper.readTree(this.body);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+			json1 = null;
+		}
+
+		this.json = json1;
 	}
 
 	public int getStatusCode() {
@@ -30,7 +44,7 @@ public class Response {
 		return body;
 	}
 
-	public JSONObject getJson() {
+	public JsonNode getJson() {
 		return this.json;
 	}
 }
